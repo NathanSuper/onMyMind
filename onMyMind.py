cfa67e3e9aaab1db033b8f0ea6248110abd0694d
NathanSuper <38273556+NathanSuper@users.noreply.github.com>
2019-05-11 21:35:50 -0700

    Add files via upload

---
+++ onMyMind.py
@@ -0,0 +1,32 @@
+import shelve, pprint
+
+print('what is on your mind?')
+currentThought = str(input())
+
+
+shelfFile = shelve.open('stuffOnMind')
+
+shelfThoughts  = shelfFile['thoughts']
+#shelfFile['thoughts'] = shelfThoughts
+#print(shelfThoughts)
+shelfThoughts.append(currentThought)
+
+print('would you like to see you past inputs? (yes = Y)')
+more = str(input())
+
+if more == 'Y':
+    pprint.pprint(shelfThoughts)
+
+shelfFile['thoughts'] = shelfThoughts
+shelfFile.close()
+
+
+'''
+shelfFile = shelve.open('stuffOnMind')
+
+shelfFile['thoughts'] = []
+
+shelfFile['thoughts']
+
+shelfFile.close()
+'''
